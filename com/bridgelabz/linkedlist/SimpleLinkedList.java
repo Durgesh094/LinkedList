9fcb347f2a900c2cda6115eb14577b707a8be8c2
Durgesh094 <durgeshchavan1994@gmail.com>
2021-12-20 20:47:44 +0530

    Added: Simple Linked List

---
+++ com/bridgelabz/linkedlist/SimpleLinkedList.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.linkedlist;
+
+import java.util.LinkedList;
+
+public class SimpleLinkedList {
+	public static void main(String args[]){ 
+		 
+		  LinkedList<Integer> l1=new LinkedList<Integer>();  
+		  l1.add(56);  
+		  l1.add(30);  
+		  l1.add(70);  
+		  
+		System.out.println(l1);
+	}    
+      
+}
