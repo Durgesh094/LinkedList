e47102047fb52801dd0e3921020d72a3de0adbeb
Durgesh094 <durgeshchavan1994@gmail.com>
2021-12-22 19:06:36 +0530

    Added: Delete First Node in Linked List

---
+++ com/bridgelabz/linkedlist/DeleteFirstNode.java
@@ -0,0 +1,50 @@
+package com.bridgelabz.linkedlist;
+
+public class DeleteFirstNode {
+	Node head;
+
+	public void insert(int data) {
+		Node nodeinsert = new Node();
+		nodeinsert.data = data;
+		nodeinsert.next = null;
+
+		if (head == null) {
+			head = nodeinsert;
+		} else {
+			Node n = head;
+			while (n.next != null) {
+				n = n.next;
+			}
+			n.next = nodeinsert;
+		}
+	}
+	
+	//Insert node between head and tail
+	public void insertLinkedList(int index, int data)
+	{
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+        Node n = head;
+        for(int i=0; i<index-1; i++) {
+        	n = n.next;
+        }
+        node.next=n.next;
+        n.next=node;
+	}
+	//Delete First Node
+	public void deleteFirstNode(int index) {
+		if(index==0) {
+			head = head.next;
+		}
+	}
+	// printing node
+	public void print() {
+		Node node = head;
+		while (node != null) {
+			System.out.print(node.data + "->");
+			node = node.next;
+		}
+
+	}
+}
